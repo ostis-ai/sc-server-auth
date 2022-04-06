b8380c4c58ceeeaa03f01318bb63dc341f9b23e5
Shtress <stas.butrin@mail.ru>
2022-04-06 12:18:23 +0300

    fix: update run server params

--- run_server.sh
+++ run_server.sh
@@ -3,9 +3,7 @@
 run_server()
 {
   fi=${host:-127.0.0.1}
-  fp=${port:-8000}
-  pi=${platform_ip:-127.0.0.1}
-  pp=${platform_port:-8090}
+  fp=${port:-5000}
   python3 -m uvicorn main:app --reload --host "$fi" --port "$fp" 
 }
 
@@ -13,24 +11,18 @@ help()
 {
    echo "i     ip address of a flask server (default value is 127.0.0.1)"
    echo "p     port of a flask server (default value is 5000)"
-   echo "s     ip address of a platform sc-server (default value is 127.0.0.1)"
-   echo "c     port of a platform sc-server(default value is 8090)"
    echo ""
    echo "h     print this help"
    echo
 }
 
-while getopts "i:p:s:c:h" option; do
+while getopts "i:p:h" option; do
    case $option in
       i)
          host=$OPTARG;;
       p)
          port=$OPTARG;;
       s)
-         platform_ip=$OPTARG;;
-      c)
-         platform_port=$OPTARG;;
-      h)
          help
          exit;;
       \?) # incorrect option
