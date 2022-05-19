3ab9ade5a88e167c9121dc44100c9d28f68eb64a
Shtress <stas.butrin1331@gmail.com>
2022-05-19 12:35:25 +0300

    fix: remove author from log.py

--- sc-server-auth/log.py
+++ sc-server-auth/log.py
@@ -1,8 +1,3 @@
-"""
-    Copyright (c) 2022 Intelligent Semantic Systems LLC, All rights reserved.
-    Author Alexandr Zagorskiy
-"""
-
 import logging
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
