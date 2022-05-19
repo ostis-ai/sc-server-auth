46f6e07596cd350f5b7fad4211caafb1b74e8966
Shtress <stas.butrin1331@gmail.com>
2022-05-19 12:45:01 +0300

    fix: remove sc-server connection from maint

--- sc-server-auth/main.py
+++ sc-server-auth/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
-
 from config import BASE_SC_SERVER_URL
-from json_client import client
 from server import admin, auth
 from log import get_default_logger
 
@@ -12,10 +10,3 @@ app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(admin.router)
-
-client.connect(BASE_SC_SERVER_URL)
-if client.is_connected():
-    log.info("Connection with sc-server established successfully")
-    client.disconnect()
-else:
-    log.warning("Connection with sc-server not established. Run auth-server in DB-only mode")
