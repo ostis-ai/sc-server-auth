b744dba73dd841a6f8dfd4d7e5a792a95b506801
KernTHeAce <vanatuz2@gmail.com>
2022-07-04 18:01:14 +0300

    fix(ci): tests, pylint, isort and black checks

---
+++ sc-server-auth/scripts/local_ci.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+unittest() {
+  echo "--------------------------------------UNITTEST--------------------------------------"
+  TEST_FLAG=true poetry run coverage run --source=. -m unittest discover -s .
+  echo "------------------------------------------------------------------------------------"
+}
+
+coverage() {
+  COVERAGE_LIMIT=90
+  echo "--------------------------------------COVERAGE--------------------------------------"
+  poetry run coverage report --fail-under=$COVERAGE_LIMIT -m
+  echo "------------------------------------------------------------------------------------"
+}
+
+isort() {
+  echo "--------------------------------------ISORT--------------------------------------"
+  poetry run isort . --check --diff --line-length 120
+  echo "---------------------------------------------------------------------------------"
+
+}
+
+black() {
+  echo "--------------------------------------BLACK--------------------------------------"
+  poetry run black --diff --color --line-length 120 .
+  echo "---------------------------------------------------------------------------------"
+}
+
+pylint() {
+  echo "--------------------------------------PYLINT--------------------------------------"
+  poetry run pylint sc-server-auth
+  echo "----------------------------------------------------------------------------------"
+}
+
+help() {
+   echo "Local-CI is a special tool to check your code before pushing"
+   echo
+   echo "Syntax: scripts/local_ci.sh -[t | c | i | b | p | a | h]"
+   echo "options:"
+   echo "t     Run unittest"
+   echo "c     Run coverage utility"
+   echo "i     Run isort"
+   echo "b     Run black"
+   echo "p     Run pylint"
+   echo "a     Run all checks"
+   echo "h     Print this help"
+   echo
+}
+
+if [ $# -lt 1 ]
+then
+  echo "No options found!"
+  exit 1
+fi
+
+while getopts "tcibpah" option; do
+   case $option in
+      t)
+         unittest;;
+      c)
+         coverage;;
+      i)
+         isort;;
+      b)
+         black;;
+      p)
+         pylint;;
+      a)
+         unittest
+         coverage
+         isort
+         black
+         pylint;;
+      h) # display Help
+         help
+         exit;;
+      \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
