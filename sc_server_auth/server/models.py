9d137de90ede47eb92dd7c5402f82b421a2eaf08
KernTHeAce <vanatuz2@gmail.com>
2022-07-21 13:57:58 +0300

    refactor(server): isort and black

--- sc_server_auth/server/models.py
+++ sc_server_auth/server/models.py
@@ -4,6 +4,8 @@ from typing import Optional
 
 import jwt
 from fastapi import HTTPException
+from google.auth.transport import requests
+from google.oauth2 import id_token
 from pydantic import BaseModel
 from pydantic.class_validators import validator
 from pydantic.dataclasses import dataclass
@@ -46,7 +48,7 @@ from google.auth.transport import requests
 def _validate_google_token(value):
     try:
         with open(params[cnt.GOOGLE_CLIENT_SECRET]) as file:
-            client_id = json.loads(file.read())['installed']["client_id"]
+            client_id = json.loads(file.read())["installed"]["client_id"]
             id_token.verify_oauth2_token(value, requests.Request(), client_id)
     except Exception:
         raise HTTPException(status_code=403, detail=params[cnt.MSG_ACCESS_DENIED])
